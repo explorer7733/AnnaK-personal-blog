a518b4aa3c12d94678a8b55e93a16fd567b4a14b
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-04-20 16:27:57 -0700

    add JS addEventListener and function for the form at form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,19 @@
-/*add addEventListerner for free inputs */
-
-
-
-
 /*JS for submit button*/
+
+const submitButton = document.querySelector('.submit-button');
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let userName = document.getElementById('username').value;
+    let title = document.getElementById('title').value;
+    let content = document.getElementById('content').value;
+
+    let completedForm = document.querySelector(".form-container");
+
+    if (userName === '' || title === '' || content === '') {
+        completedForm.textContent = 'Please complete the form';
+    } else {
+        completedForm.textContent = 'Submitted';
+    }
+});
